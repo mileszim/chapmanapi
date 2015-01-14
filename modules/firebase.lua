f5a5877b786dffce9308731748007ace3bfef275
Miles Zimmerman <miles@maztek.io>
2015-01-14 03:54:05 -0800

    Updating Firebase API

--- modules/firebase.lua
+++ modules/firebase.lua
@@ -1,90 +1,103 @@
 -- Firebase module for Webscript.io
+--
+-- To use:
+-- local firebase = require('mileszim/chapmanapi/modules/firebase.lua')
+-- firebase.init({
+--   root = 'https://my-firebase-ref.firebaseio.com,
+--   auth = 'my-auth-token'
+-- })
 
+
+-- Namespace
 local Firebase = {}
 
+
+-- API
 function Firebase:nodePath(node)
   return self.root .. '/' .. node .. '.json'
 end
 
+
 function Firebase:get(node)
   local req = http.request {
-	  method = 'GET',
-	  url = self:nodePath(node),
-	  params = {
-	    auth = self.authtoken
-	  }
-	}
-	
-	if req.statuscode == 200 then
-	  return json.parse(req.content)
-	else
-	  return req.statuscode
-	end
+    method = 'GET',
+    url = self:nodePath(node),
+    params = {
+      auth = self.authtoken
+    }
+  }
+  if req.statuscode == 200 then
+    return json.parse(req.content)
+  else
+    return req.statuscode
+  end
 end
+
 
 function Firebase:set(node, content)
   local req = http.request {
-	  method = 'PUT',
-	  url = self:nodePath(node),
-	  params = {
-	    auth = self.authtoken
-	  },
-	  data = content
-	}
-	
-	if req.statuscode == 200 then
-	  return json.parse(req.content)
-	else
-	  return req.statuscode
-	end
+    method = 'PUT',
+    url = self:nodePath(node),
+    params = {
+      auth = self.authtoken
+    },
+    data = content
+  }
+  if req.statuscode == 200 then
+    return json.parse(req.content)
+  else
+    return req.statuscode
+  end
 end
+
 
 function Firebase:push(node, content)
   local req = http.request {
-	  method = 'POST',
-	  url = self:nodePath(node),
-	  params = {
-	    auth = self.authtoken
-	  },
-	  data = content
-	}
-	
-	if req.statuscode == 200 then
-	  return json.parse(req.content)
-	else
-	  return req.statuscode
-	end
+    method = 'POST',
+    url = self:nodePath(node),
+    params = {
+      auth = self.authtoken
+    },
+    data = content
+  }
+  if req.statuscode == 200 then
+    return json.parse(req.content)
+  else
+    return req.statuscode
+  end
 end
+
 
 function Firebase:update(node, content)
   local req = http.request {
-	  method = 'PATCH',
-	  url = self:nodePath(node),
-	  params = {
-	    auth = self.authtoken
-	  },
-	  data = content
-	}
-	
-	if req.statuscode == 200 then
-	  return json.parse(req.content)
-	else
-	  return req.statuscode
-	end
+    method = 'PATCH',
+    url = self:nodePath(node),
+    params = {
+      auth = self.authtoken
+    },
+    data = content
+  }
+  if req.statuscode == 200 then
+    return json.parse(req.content)
+  else
+    return req.statuscode
+  end
 end
+
 
 function Firebase:remove(node)
   local req = http.request {
-	  method = 'DELETE',
-	  url = self:nodePath(node),
-	  params = {
-	    auth = self.authtoken
-	  }
-	}
-	
-	return req.statuscode
+    method = 'DELETE',
+    url = self:nodePath(node),
+    params = {
+      auth = self.authtoken
+    }
+  }
+  return req.statuscode
 end
 
+
+-- Instance Handling
 function Firebase:new(o)
   o = o or {}
   setmetatable(o, self)
@@ -92,10 +105,11 @@ function Firebase:new(o)
   return o
 end
 
-
--- Export
-local function init(params)
-  return Firebase:new(params)
+function Firebase:init(params)
+  self.root = params.root
+  self.auth = params.auth
 end
 
-return { init = init }
+
+-- Export
+return Firebase:new()
